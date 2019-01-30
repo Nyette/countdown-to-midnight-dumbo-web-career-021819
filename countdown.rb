e718805152dc894a5c6ac0ed31e7e06f39ab8154
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-30 04:03:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 # write your code here
 
-def countdown(num)
-  while num > 0
-    num -= 1
-    puts "#{num} SECOND(S)!"
+def countdown(number)
+  while number > 0
+    number -= 1
+    puts "#{number} SECOND(S)!"
   end
   return "HAPPY NEW YEAR!"
 end
